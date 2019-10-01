b1298608cd73d4d7d9d2d1450b60ffec8398b460
Paulo Henrique dos Santos Luz <paulohenrique.luz@hotmail.com>
2019-10-01 20:56:13 -0300

    Checkpoint aula 01/10/2019

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform alvo;            
+    public float suavidade = 5f;        
+
+    Vector3 distanciaPadrao;                     
+
+    void Start ()
+    {
+        
+        distanciaPadrao = transform.position - alvo.position;
+    }
+
+    void FixedUpdate ()
+    {
+        
+        Vector3 targetCamPos = alvo.position + distanciaPadrao;
+
+        
+        transform.position = Vector3.Lerp (transform.position, targetCamPos, suavidade * Time.deltaTime);
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;            
+
+    Vector3 movimento;                   
+    Animator animator;                      
+    Rigidbody playerRigidbody;          
+    int floorMask;                      
+    float distanciaRaioCamera = 100f;          
+
+   	public Camera cam;
+
+
+    void Awake ()
+    {
+        
+        floorMask = LayerMask.GetMask ("Floor");
+
+
+        animator = GetComponent <Animator> ();
+        playerRigidbody = GetComponent <Rigidbody> ();
+		cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+
+    void FixedUpdate ()
+    {
+
+        float h = Input.GetAxisRaw ("Horizontal");
+        float v = Input.GetAxisRaw ("Vertical");
+
+
+        Movimenta (h, v);
+
+
+        Gira ();
+
+
+        Anima (h, v);
+    }
+
+    void Movimenta (float h, float v)
+    {
+
+        movimento.Set (h, 0f, v);
+        
+
+        movimento = movimento.normalized * speed * Time.deltaTime;
+
+
+        playerRigidbody.MovePosition (transform.position + movimento);
+    }
+
+    void Gira ()
+    {
+
+        Ray camRay = cam.ScreenPointToRay (Input.mousePosition);
+
+
+        RaycastHit tocaChao;
+
+
+        if(Physics.Raycast (camRay, out tocaChao, distanciaRaioCamera, floorMask))
+        {
+
+            Vector3 playerToMouse = tocaChao.point - transform.position;
+
+
+            playerToMouse.y = 0f;
+
+
+            Quaternion novaRotacao = Quaternion.LookRotation (playerToMouse);
+
+
+            playerRigidbody.MoveRotation (novaRotacao);
+        }
+    }
+
+    void Anima (float h, float v)
+    {
+
+        bool anda = h != 0f || v != 0f;
+
+
+        animator.SetBool ("IsWalking", anda);
+    }
+}
+
